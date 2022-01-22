f6ad266cc0513357aee9133b36b78d50bcf4b2f0
Ramsey Njire <ramseynjire@gmail.com>
2022-01-22 12:17:14 +0300

    write placePiece method

--- src/board.js
+++ src/board.js
@@ -134,6 +134,28 @@ Board.prototype.validMove = function (pos, color) {
  * Throws an error if the position represents an invalid move.
  */
 Board.prototype.placePiece = function (pos, color) {
+  if(!(this.validMove(pos, color))){
+    throw new Error('Invalid move!');
+  }
+
+  const [x, y] = pos;
+
+  this.grid[x][y] = new Piece(color);
+
+  const piecesToFlip = [];
+
+  for (let i = 0; i < Board.DIRS.length; i++) {
+    let pieceArray = this._positionsToFlip(pos, color, Board.DIRS[i]);
+    if(pieceArray.length){
+      piecesToFlip.push(pieceArray);
+    }
+  }
+
+  for (let i = 0; i < piecesToFlip.length; i++) {
+    for (let j = 0; j < piecesToFlip[i].length; j++) {
+      this.getPiece(piecesToFlip[i][j]).flip();
+    }
+  }
 };
 
 /**
